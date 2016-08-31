854b4150dc6a255ba2bbfe97767f710084c440d9
Ryan Travitz <rtravitz@gmail.com>
2016-08-31 17:56:26 -0600

    MVP for GUI app

--- lib/complete_me_shoes.rb
+++ lib/complete_me_shoes.rb
@@ -1,5 +1,4 @@
 require_relative "complete_me"
-# require_relative "../usr/share/dict/words"
 
 trie = CompleteMe.new
 dictionary = File.read("../usr/share/dict/words")
@@ -10,17 +9,48 @@ Shoes.app do
   background "#b3d9ff"
   border("#0059b3", strokewidth: 4)
 
-  stack(margin: 8) do
-    para "Enter a partial word to autocomplete:"
-    flow do
-      @edit_line = edit_line
-      @button = button "Search" do
-        substring = @edit_line.text
-        suggestions = trie.suggest(substring)
-        suggestions = suggestions.join(", ")
-        @results.remove if @results
-        @results = para suggestions
+  flow do
+
+    stack margin: 10, :width => "50%" do
+      @results = para
+    end
+
+    stack margin: 10, :width => "-50%" do
+      para "Enter a partial word to complete:"
+
+      flow do
+        @autocomplete_line = edit_line
+        @autcomplete_button = button "Search" do
+          substring = @autocomplete_line.text
+          suggestions = trie.suggest(substring)
+          suggestions = suggestions.join(", ")
+          @results.replace(suggestions)
+        end
       end
+      para ""
+      para "Enter a word to delete:"
+
+      flow do
+        @delete_line = edit_line
+        @delete_button = button "Delete" do
+          word = @delete_line.text
+          trie.delete(word)
+          @results.replace("#{word.capitalize} has been deleted.")
+        end
+      end
+
+      para ""
+      para "Enter a substring and a
+word separated by a space:"
+      flow do
+        @select_line = edit_line
+        @select_button = button "Select" do
+          words = @select_line.text.split
+          trie.select(words.first, words.last)
+          @results.replace("#{words.last.capitalize} will return as a priority when you enter #{words.first}!")
+        end
+      end
+
     end
   end
 end
